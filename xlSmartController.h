a767069097acd7f46ccfda162067a856f4e24aa9
Jerry Bai <jerrybai1@hotmail.com>
2016-06-08 18:56:30 -0400

    Overloaded LinkedList add functions in ChainClass, added more comments to alarms feature

--- xlSmartController.h
+++ xlSmartController.h
@@ -80,9 +80,9 @@ public:
 
   //LinkedLists (Working memory tables)
   DevStatus_t DevStatus_row;
-  ChainClass<ScheduleRow_t> Schedule_table = ChainClass<ScheduleRow_t>();
-  ChainClass<RuleRow_t> Rule_table = ChainClass<RuleRow_t>();
-  ChainClass<ScenarioRow_t> Scenario_table = ChainClass<ScenarioRow_t>();
+  ChainClass<ScheduleRow_t> Schedule_table = ChainClass<ScheduleRow_t>(PRE_FLASH_MAX_TABLE_SIZE);
+  ChainClass<RuleRow_t> Rule_table = ChainClass<RuleRow_t>(PRE_FLASH_MAX_TABLE_SIZE);
+  ChainClass<ScenarioRow_t> Scenario_table = ChainClass<ScenarioRow_t>(PRE_FLASH_MAX_TABLE_SIZE);
 
 protected:
   // Communication Interfaces
